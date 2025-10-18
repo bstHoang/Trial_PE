61c2a463987e6d376d4737f9d6bb3c700b6f8005
Bui Sy Thai Hoang <10t1qx4thaihoang@gmail.com>
2025-10-18 15:51:51 +0700

    Hoang add answer for Trial PE VI

---
+++ Hoang/VI/Given/Project11/Models/Star.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Project11.Models;
+
+public partial class Star
+{
+    [Key]
+    [Column("StarID")]
+    public int StarId { get; set; }
+
+    [StringLength(50)]
+    public string? FirstName { get; set; }
+
+    [StringLength(50)]
+    public string? LastName { get; set; }
+
+    public DateOnly? BirthDate { get; set; }
+
+    [StringLength(10)]
+    public string? Gender { get; set; }
+
+    [InverseProperty("Star")]
+    public virtual ICollection<MovieStar> MovieStars { get; set; } = new List<MovieStar>();
+}
