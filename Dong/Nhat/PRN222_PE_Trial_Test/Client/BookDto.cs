cb93f3cb4201ad0831e0ebd6ef455255772bbad8
dongnuc <dongnuc2k3@gmail.com>
2025-10-19 10:30:28 +0700

    solution nhat

---
+++ Dong/Nhat/PRN222_PE_Trial_Test/Client/BookDto.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+
+namespace Client
+{
+    public class BookDto
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public int PublicationYear { get; set; }
+        public string Genres { get; set; }
+        public List<AuthorDto> Authors { get; set; }
+    }
+
+    public class AuthorDto
+    {
+        public string Name { get; set; }
+        public int BirthYear { get; set; }
+    }
+}
